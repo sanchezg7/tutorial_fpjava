e79e50d46f0806c51835ae11de34eafd6073d189
ayogsynergy <sanchezg.eng@gmail.com>
2023-01-11 19:53:38 -0500

    with fp approach

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,25 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        printAllNumbersInListStructure(List.of(12,9,13,4,6,2,4,12,15));
+        List<Integer> integers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+//        printAllNumbersInListStructureItr(integers);
+        printAllNumDeclarative(integers);
+
     }
 
-    private static void printAllNumbersInListStructure(List<Integer> numbers) {
+    private static void printAllNumbersInListStructureItr(List<Integer> numbers) {
         // loop the numbers
         for(int num : numbers) {
             System.out.println(num);
         }
+    }
+
+    private static void printAllNumDeclarative(List<Integer> numbers) {
+        numbers.stream() // turn into a sequence of numbers
+                .forEach(Main::print); // method reference
+    }
+
+    private static void print(int number) {
+        System.out.println(number);
     }
 }
