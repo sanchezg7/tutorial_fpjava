fa59bfb78f9c41f5342989fc3d66e1c686363cc9
ayogsynergy <sanchezg.eng@gmail.com>
2023-01-11 20:29:09 -0500

    with lambda and tinkering with a bunch of intellij wsl settings

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,55 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         List<Integer> integers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
-//        printAllNumbersInListStructureItr(integers);
-        printAllNumDeclarative(integers);
+//        Iterative.printAllNumbersInListStructureItr(integers);
+//        Declarative.printAllNumDeclarative(integers);
+//        Declarative.printEvenNumbers(integers);
+        Declarative.printOddNumbersWithLambda(integers);
 
     }
 
-    private static void printAllNumbersInListStructureItr(List<Integer> numbers) {
-        // loop the numbers
-        for(int num : numbers) {
-            System.out.println(num);
+    public static class Iterative {
+        private static void printAllNumbersInListStructureItr(List<Integer> numbers) {
+            // loop the numbers
+            for (int num : numbers) {
+                System.out.println(num);
+            }
+        }
+
+        private static void printEvenNumbersInList(List<Integer> numbers) {
+            for (int num : numbers) {
+                if (num % 2 == 0) {
+                    System.out.println(num);
+                }
+            }
         }
     }
 
-    private static void printAllNumDeclarative(List<Integer> numbers) {
-        numbers.stream() // turn into a sequence of numbers
-                .forEach(Main::print); // method reference
+    public static class Declarative {
+        private static void print(int number) {
+            System.out.println(number);
+        }
+        private static boolean isEven(int number) {
+            return number % 2 == 0;
+        }
+        private static void printAllNumDeclarative(List<Integer> numbers) {
+            numbers.stream() // turn into a sequence of numbers
+                    .forEach(Declarative::print); // method reference
+        }
+
+
+        private static void printEvenNumbers(List<Integer> numbers) {
+            numbers.stream()
+                    .filter(Declarative::isEven)
+                    .forEach(System.out::println);
+        }
+
+        private static void printOddNumbersWithLambda(List<Integer> numbers) {
+            numbers.stream()
+                    .filter(num -> num % 2 != 0)
+                    .forEach(System.out::println);
+        }
     }
 
-    private static void print(int number) {
-        System.out.println(number);
-    }
 }
